f23c01e07f841bed38bc802427d23b8042c2959c
WelcomeDG <welcome5190@hotmail.com>
2023-03-06 22:23:22 +0800

    another test

--- libs/ntag215/jswrap_ntag215.c
+++ libs/ntag215/jswrap_ntag215.c
@@ -475,14 +475,13 @@ void jswrap_ntag215_setAmiiboKeys(JsVar *v){
 }*/
 JsVar *jswrap_ntag215_unpackAmiibo(JsVar *v){
  	uint8_t modified[NFC3D_AMIIBO_SIZE];
-  memcpy(&modified, v, sizeof(amiiboKeys));
-//   size_t len=0;
-//   char *original = jsvGetDataPointer(v, &len);
-//   //bool result = nfc3d_amiibo_unpack(&amiiboKeys, original, modified);
+  size_t len=0;
+  uint8_t *original = jsvGetDataPointer(v, &len);
+  bool result = nfc3d_amiibo_unpack(&amiiboKeys, original, modified);
   
-//   //jsiConsolePrintf("result: %d", result);
-//   //jsiConsolePrintf("result: %d", modified[0]);
-//   //jsiConsolePrintf("result: %d", sizeof(modified));
+  jsiConsolePrintf("result: %d", result);
+  jsiConsolePrintf("result: %d", modified[0]);
+  jsiConsolePrintf("result: %d", sizeof(modified));
   return jsvNewArrayFromBytes(modified, sizeof(modified));
 }
 #endif
